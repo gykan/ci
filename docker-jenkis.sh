0712bf215f01183226e413ed155c48ccbdb5fa31
Andrei Grigorev <andrei.grigorev@sharecare.com>
2017-03-28 15:40:08 -0400

    added DRY lib

--- docker-jenkis.sh
+++ docker-jenkis.sh
@@ -2,6 +2,7 @@
 
 docker run -p 8080:8080 -p 5000:5000 -v /tmp/jenkins-home:/var/jenkins_home \
         -e ORG_NAME='<GITHUB ORG NAME>' \
+        -e BUILD_LIB_REPO='<repo with a build script library, DEFAULT is ci' \
         -e AUTH_CLIENT_ID='<GITHUB AUTH ID: https://wiki.jenkins-ci.org/display/JENKINS/Github+OAuth+Plugin>' \
         -e AUTH_CLIENT_SECRET='<GITHUB AUTH SECRET>' \
         -e GIT_USER_NAME='<GITHUB USERNAME>' \
