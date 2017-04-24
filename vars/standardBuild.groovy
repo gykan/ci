a9df98559d5c2a9bdf7e981f99e74d16e43aa3ce
Andrei Grigorev <Andrei.Grigorev@arris.com>
2017-04-24 15:32:20 -0400

    new version

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -3,65 +3,140 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    node {
-        stage('Checkout') {
-            checkout scm
-        }
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'artifactory-credentials',
-                          usernameVariable: 'ARTUSERNAME', passwordVariable: 'ARTPASSWORD'],
-                         [$class: 'UsernamePasswordMultiBinding', credentialsId: 'aws-credentials',
-                          usernameVariable: 'AWS_ID', passwordVariable: 'AWS_KEY']]) {
+    withCredentials(
+            [
+                    [
+                            $class          : 'UsernamePasswordMultiBinding',
+                            credentialsId   : 'artifactory-credentials',
+                            usernameVariable: 'ARTUSERNAME',
+                            passwordVariable: 'ARTPASSWORD'
+                    ],
+                    [
+                            $class          : 'UsernamePasswordMultiBinding',
+                            credentialsId   : 'aws-credentials',
+                            usernameVariable: 'AWS_ACCESS_KEY',
+                            passwordVariable: 'AWS_SECRET_ACCESS_KEY'
+                    ]
+            ]
+    ) {
+        def serviceName = readFile('gradle.properties') =~ 'buildContext=(.+)'
+        serviceName ? serviceName[0][1] : null
+//Lets define a unique label for this build.
+        def label = "${serviceName}.${env.JOB_NAME}.${env.BUILD_NUMBER}".replace('-', '_').replace('/', '_')
+        podTemplate(
+                label: label,
+                containers: [
+                        containerTemplate(
+                                name: 'gradle',
+                                image: 'frekele/gradle:latest',
+                                ttyEnabled: true,
+                                command: 'cat',
+                                envVars: [
+                                        containerEnvVar(key: "ARTUSERNAME", value: env.ARTUSERNAME),
+                                        containerEnvVar(key: "ARTPASSWORD", value: env.ARTPASSWORD),
+                                        containerEnvVar(key: "AWS_ACCESS_KEY", value: env.AWS_ACCESS_KEY),
+                                        containerEnvVar(key: "AWS_SECRET_ACCESS_KEY", value: env.AWS_SECRET_ACCESS_KEY)
+                                ]
+                        ),
+                        containerTemplate(
+                                name: 'docker',
+                                image: 'docker:latest',
+                                ttyEnabled: true,
+                                command: 'cat'
+                        ),
+                        containerTemplate(
+                                name: 'jnlp',
+                                image: 'jenkinsci/jnlp-slave:2.62-alpine',
+                                args: '${computer.jnlpmac} ${computer.name}'
+                        )
+                ],
+                volumes: [
+                        emptyDirVolume(
+                                mountPath: '/home/jenkins',
+                                memory: false
+                        ),
+                        hostPathVolume(
+                                mountPath: '/var/run/docker.sock',
+                                hostPath: '/var/run/docker.sock'
+                        )
+                ]
+        ) {
+            //Lets use pod template (refernce by label)
+            node(label) {
+                checkout scm
+                def options = ' -PartUsername=$ARTUSERNAME -PartPassword=$ARTPASSWORD -Daws.accessKeyId=$AWS_ACCESS_KEY -Daws.secretKey=$AWS_SECRET_ACCESS_KEY '
+                def v = version()
 
-            def options = ' -PartUsername=$ARTUSERNAME -PartPassword=$ARTPASSWORD -Daws.accessKeyId=$AWS_ID -Daws.secretKey=$AWS_KEY '
-
-            stage('Assemble') {
-                if (config.assemble != null) {
-                    if (config.assemble) {
-                        sh(config.assemble + options)
-                    }
-                } else {
-                    sh "./gradlew $options clean build -x test"
+                stage 'Setup gradle'
+                container(name: 'gradle') {
+                    sh "apt-get update && apt-get install git -y"
                 }
-            }
 
-            stage('Test') {
-                if (config.test != null) {
-                    if (config.test) {
-                        sh(config.test + options)
-                    }
-                } else {
-                    sh "./gradlew $options test"
-                }
-            }
-
-            stage('Regression') {
-                if (config.regression != null) {
-                    if (config.regression) {
-                        sh(config.regression + options)
-                    }
-                } else {
-                    sh "./gradlew $options regression"
-                }
-            }
-
-            stage('Upload') {
-                if (config.upload != null) {
-                    if (config.upload) {
-                        sh(config.upload + options)
-                    }
-                } else {
-                    sh "./gradlew $options -Denv=dev s3Upload; ./gradlew $options -Denv=rc s3Upload"
-                }
-            }
-
-            stage('Deploy') {
-                if (config.deploy != null) {
-                    if (config.deploy) {
-                        sh(config.deploy + options)
+                stage 'Assemble'
+                container(name: 'gradle') {
+                    if (config.assemble != null) {
+                        if (config.assemble) {
+                            sh(config.assemble + options)
+                        }
+                    } else {
+                        sh "./gradlew $options clean build -x test"
                     }
                 }
-                else echo "Deploying..."
+
+                stage 'Test'
+                container(name: 'gradle') {
+                    if (config.test != null) {
+                        if (config.test) {
+                            sh(config.test + options)
+                        }
+                    } else {
+                        sh "./gradlew $options test"
+                    }
+                }
+
+                stage 'Regression'
+                container(name: 'gradle') {
+                    if (config.regression != null) {
+                        if (config.regression) {
+                            sh(config.regression + options)
+                        }
+                    } else {
+                        sh "./gradlew $options regression"
+                    }
+                }
+
+                stage 'Docker image'
+                docker.withRegistry(
+                        'https://index.docker.io/v1/',
+                        'docker-hub-credentials'
+                )
+                        {
+                            stage 'Docker build'
+                            container(name: 'docker') {
+                                def imageName = "nykanon/gykan:${serviceName}-latest"
+                                sh "docker build -t ${imageName} ."
+                                image = docker.image(imageName)
+                                image.push()
+                            }
+                        }
+
+                def env = env.BRANCH_NAME == 'development' ? "rc" : "dev"
+                stage 'Upload rc'
+                container(name: 'gradle') {
+                    if (config.upload != null) {
+                        if (config.upload) {
+                            sh(config.upload + " -Denv=" + env + " " + options)
+                        }
+                    } else {
+                        sh "./gradlew $options -Denv=${env} s3Upload"
+                    }
+                }
             }
         }
     }
+}
+
+def version() {
+    def matcher = readFile('gradle.properties') =~ 'version=(.+)'
+    matcher ? matcher[0][1] : null
 }
